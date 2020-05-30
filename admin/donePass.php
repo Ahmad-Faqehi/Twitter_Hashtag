b821ad834461f8e65427ad83384b6d713032f7b4
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2020-05-30 19:24:33 +0300

    Done

---
+++ admin/donePass.php
@@ -0,0 +1,156 @@
+<?php
+include("includes/init.php");
+
+if($_SESSION['change_pass'] == "done"){
+
+?>
+<html lang="ar">
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>صفحة تعديل على مستخدم</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@700&display=swap" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/process.js"></script>
+
+    <!-- Custom styles for this template-->
+    <link href="css/mystyle.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+<body id="page-top" class="sidebar-toggled">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-info sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fab fa-bitbucket"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">سالفة هاشتاق</div>
+        </a>
+
+        <!-- Divider -->
+
+        <!-- Nav Item - Dashboard -->
+
+        <!-- <i class="fal fa-"></i> -->
+        <!-- Divider -->
+
+        <!-- Heading -->
+        <div class="sidebar-heading font-weight-bold Font-tajawal">
+            الخيارات
+        </div>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <script>
+        $('#link').click(function () {
+            <?php unset($_SESSION['change_pass']); ?>
+        });
+    </script>
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3" style="color: #36b9cc">
+                    <i class="fa fa-bars"></i>
+                </button>
+
+            </nav>
+            <!-- End of Topbar -->
+
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-info Font-tajawal text-center">أعادة تعين كلمة السر</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-center h4 Font-tajawal">
+
+                            تم تغير كلمة السر بنجاح
+                            <a href="login.php" class="btn-link" id="link"> <small class="h6 text-info"> تسجيل دخول</small></a>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- /.container-fluid -->
+
+
+        </div>
+        <!-- End of Main Content -->
+
+
+        <!-- Footer -->
+
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; سالفة هاشتاق 2020</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Logout Modal-->   <!-- Scroll to Top Button-->
+
+
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+
+<!-- Secript Write here -->
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script> <!--/ Secript Write here -->
+</body>
+
+</html>
+<?php }else{ redirect("login.php"); } ?>
